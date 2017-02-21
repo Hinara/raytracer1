53cf1587af366276e5e894ebce0c3323a391f6d8
robbix1206 <robbix1206@gmail.com>
2017-02-21 18:21:45 +0100

    Correct translate

--- src/translate.c
+++ src/translate.c
@@ -5,13 +5,21 @@
 ** Login   <robin.milas@epitech.net>
 ** 
 ** Started on  Mon Feb 20 16:59:03 2017 Robin MILAS
-** Last update Mon Feb 20 17:00:09 2017 Robin MILAS
+** Last update Tue Feb 21 18:21:36 2017 Robin MILAS
 */
 
 sfVector3f translate(sfVector3f to_translate, sfVector3f translations)
 {
-  to_translate.x = translations.x;
-  to_translate.y = translations.y;
-  to_translate.z = translations.z;
-  return (to_translate.z);
+  to_translate.x += translations.x;
+  to_translate.y += translations.y;
+  to_translate.z += translations.z;
+  return (to_translate);
+}
+
+sfVector3f rev_translate(sfVector3f to_translate, sfVector3f translations)
+{
+  to_translate.x -= translations.x;
+  to_translate.y -= translations.y;
+  to_translate.z -= translations.z;
+  return (to_translate);
 }
