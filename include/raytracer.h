e7f038ee711b8b3fc3c152a6a7892554c3f5f0a8
robbix1206 <robbix1206@gmail.com>
2017-03-11 04:15:51 +0100

    Clean a bit be 'faster better stronger'

--- include/raytracer.h
+++ include/raytracer.h
@@ -5,25 +5,46 @@
 ** Login   <robin.milas@epitech.net>
 ** 
 ** Started on  Wed Feb  8 11:05:14 2017 Robin MILAS
-** Last update Sat Mar 11 00:10:19 2017 Milas Robin
+** Last update Sat Mar 11 04:14:43 2017 Milas Robin
 */
 
 #ifndef _RAYTRACER_H_
 # define _RAYTRACER_H_
 
-# include <math.h>
+/*
+** Necessary include for prototypes
+*/
 # include <SFML/Graphics.h>
 
+/*
+** Framebuffer dimensions
+*/
 # define FRAMEBUFFER_WIDTH	800
 # define FRAMEBUFFER_HEIGHT	600
 
+/*
+** Small define to convert degrees in radius
+*/
 # define RAD(x)	((x) * M_PI / 180)
 
+/*
+** Default color when there no collide beetween a ray and all objects
+*/
 # define SKY	sfBlack
 
+/*
+** Distance beetween the center camera point and the screen plane
+*/
 # define DIST			500
+
+/*
+** Rotation factor when a player rotate the camera
+*/
 # define ROTATE_FACTOR		10
 
+/*
+** A enum of all the available shapes
+*/
 typedef enum	e_shape_type
 {
   SPHERE,
@@ -34,15 +55,19 @@ typedef enum	e_shape_type
 }		t_shape_type;
 
 /*
-** CSFML Structures
+** Framebuffer structure
 */
 typedef struct	s_my_framebuffer
 {
   sfUint8	*pixels;
   int		width;
   int		height;
-}		t_my_framebuffer;
+}
+  t_my_framebuffer;
 
+/*
+** CSFML Data structure
+*/
 typedef struct		s_win
 {
   t_my_framebuffer	fb;
@@ -55,18 +80,28 @@ typedef struct		s_win
 ** Scene structures
 */
 
+/*
+** Contain the shape and the color of the object
+*/
 typedef struct	s_shape
 {
   t_shape_type	shape;
   sfColor	color;
 }		t_shape;
 
+/*
+** Contains the position and the rotation of the object
+*/
 typedef struct	s_coord3d
 {
   sfVector3f	pos;
   sfVector3f	rot;
 }		t_coord3d;
 
+/*
+** The object structure
+** More can be used to stock information specific to the shape
+*/
 typedef struct	s_obj
 {
   t_shape	shape;
@@ -75,6 +110,12 @@ typedef struct	s_obj
   struct s_obj	*next;
 }		t_obj;
 
+/*
+** The scene structure
+** Contain the camera coordinates
+** The light position
+** The first object of the chained list
+*/
 typedef struct	s_scene
 {
   t_coord3d	cam;
@@ -82,60 +123,37 @@ typedef struct	s_scene
   sfVector3f	light;
 }		t_scene;
 
+/*
+** Define a type for intersections decoders
+*/
 typedef float (*t_intersect)(sfVector3f *pos,
 			     sfVector3f *dir, t_obj *obj);
 
+/*
+** Define a type for normals decoders
+*/
 typedef sfVector3f (*t_normal)(sfVector3f *pos,
 			       sfVector3f *dir, t_obj *obj, float k) ;
 
+/*
+** Use to make an array of pointer with shape type as index
+*/
 typedef struct	s_obj_dec
 {
   t_shape_type	type;
   t_intersect	intersect;
   t_normal	normal;
 }		t_obj_dec;
-/*
-** PROTOTYPE A TRIER
-*/
-float	get_light_coef(sfVector3f light_vector, sfVector3f normal_vector);
-int			my_print(char *str, int fd, int res);
-int			init_scene(t_scene *scene, t_coord3d cam);
-
-void			my_put_pixel(t_my_framebuffer *buf, int x,
-				     int y, sfColor color);
-int			window(t_scene *scene, t_win *win);
-void			raytracer(t_scene *scene, t_win *win);
-t_my_framebuffer	my_framebuffer_create(int width, int height);
-sfColor			color_brightness(sfColor color, float multiplier);
-float			intersection(double a, double b, double c);
-double			rad(double x);
-sfVector3f	vector_create(float x, float y, float z);
-sfVector3f	vector_multiply(sfVector3f vec, float k);
-sfVector3f	vector_move(sfVector3f org, sfVector3f dir, float k);
-sfVector3f	vector_reverse(sfVector3f vec);
 
 /*
-** Add forms
+** Translations
 */
-int	add_sphere(t_scene *scene, t_coord3d base,
-		   sfColor color, float radius);
-int	add_cylinder(t_scene *scene, t_coord3d base,
-		     sfColor color, float radius);
-int	add_cone(t_scene *scene, t_coord3d base,
-		 sfColor color, float angle);
-int	add_plane(t_scene *scene, t_coord3d base,
-		  sfColor color);
-
-/*
-** Transformation
-*/
-sfVector3f	calc_dir_vector(float dist_to_plane,
-				sfVector2i iscreen_size,
-				sfVector2i screen_pos);
 sfVector3f	translate(sfVector3f to_translate, sfVector3f translations);
 sfVector3f	rev_translate(sfVector3f to_translate,
 			      sfVector3f translations);
-
+/*
+** Rotations
+*/
 sfVector3f	rotate_xyz(sfVector3f to_rotate, sfVector3f angles);
 sfVector3f	rotate_zyx(sfVector3f to_rotate, sfVector3f angles);
 
@@ -165,6 +183,9 @@ sfVector3f	get_normal_cone(sfVector3f intersection_point,
 t_intersect	intersect_decoder(t_shape_type type);
 t_normal	normal_decoder(t_shape_type type);
 
+/*
+** Intersect decoder
+*/
 float	decoder_sphere_intersect(sfVector3f *pos,
 				 sfVector3f *dir, t_obj *obj);
 float	decoder_cone_intersect(sfVector3f *pos,
@@ -174,6 +195,9 @@ float	decoder_cylinder_intersect(sfVector3f *pos,
 float	decoder_plane_intersect(sfVector3f *pos,
 				sfVector3f *dir, t_obj *obj);
 
+/*
+** Normal decoder
+*/
 sfVector3f	decoder_sphere_normal(sfVector3f *pos, sfVector3f *dir,
 				      t_obj *obj, float k);
 sfVector3f	decoder_cone_normal(sfVector3f *pos, sfVector3f *dir,
@@ -182,10 +206,74 @@ sfVector3f	decoder_cylinder_normal(sfVector3f *pos, sfVector3f *dir,
 					t_obj *obj, float k);
 sfVector3f	decoder_plane_normal(sfVector3f *pos, sfVector3f *dir,
 				     t_obj *obj, float k);
+
 /*
 ** Controls
 */
 char	moves(t_scene *scene);
 char    rotate(t_scene *scene);
+
+/*
+** Manage light
+*/
+float	get_light_coef(sfVector3f light_vector, sfVector3f normal_vector);
+
+/*
+** Vectors operations
+*/
+sfVector3f	vector_move(sfVector3f org, sfVector3f dir, float k);
+sfVector3f	vector_create(float x, float y, float z);
+sfVector3f	vector_multiply(sfVector3f vec, float k);
+sfVector3f	vector_reverse(sfVector3f vec);
+
+/*
+** Add object functions
+*/
+int	add_sphere(t_scene *scene, t_coord3d base, sfColor color, float radius);
+int	add_cylinder(t_scene *scene, t_coord3d base,
+		     sfColor color, float radius);
+int	add_cone(t_scene *scene, t_coord3d base, sfColor color, float angle);
+int	add_plane(t_scene *scene, t_coord3d base, sfColor color);
+
+/*
+** Scene function
+*/
+int	init_scene(t_scene *scene, t_coord3d cam);
+
+/*
+** Math util function
+*/
+float                  intersection(double a, double b, double c);
+
+/*
+** Light function
+*/
+float  get_light_coef(sfVector3f light_vector, sfVector3f normal_vector);
+
+/*
+** Util functions
+*/
+int                    my_print(char *str, int fd, int res);
+
+/*
+** Display functions
+*/
+void                   my_put_pixel(t_my_framebuffer *buf, int x, int y,
+				    sfColor color);
+int                    window(t_scene *scene, t_win *win);
+void                   raytracer(t_scene *scene, t_win *win);
+t_my_framebuffer       my_framebuffer_create(int width, int height);
+
+/*
+** Color operations
+*/
+sfColor			color_brightness(sfColor color, float multiplier);
+
+/*
+** Calc vector director function
+*/
+sfVector3f     calc_dir_vector(float dist_to_plane,
+			       sfVector2i iscreen_size,
+			       sfVector2i screen_pos);
 
 #endif
