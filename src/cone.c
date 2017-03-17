32c389f2052b18d4720cb1e07274716bc91d802f
robbix1206 <robbix1206@gmail.com>
2017-03-17 12:55:55 +0100

    Cone

--- src/cone.c
+++ src/cone.c
@@ -5,7 +5,7 @@
 ** Login   <robin.milas@epitech.net>
 ** 
 ** Started on  Tue Jan  3 14:36:45 2017 Robin MILAS
-** Last update Thu Mar  9 16:46:21 2017 Milas Robin
+** Last update Fri Mar 17 12:55:21 2017 Milas Robin
 */
 
 #include <math.h>
@@ -15,6 +15,13 @@
 sfVector3f	get_normal_cone(sfVector3f intersection_point,
 				float semiangle)
 {
+  if (semiangle <= 0 || semiangle >= 90)
+    {
+      intersection_point.x = 0.0f;
+      intersection_point.y = 0.0f;
+      intersection_point.z = 0.0f;
+      return (intersection_point);
+    }
   intersection_point.z *= -cos(semiangle * M_PI / 180);
   return (intersection_point);
 }
@@ -26,6 +33,10 @@ float	intersect_cone(sfVector3f pos, sfVector3f dir, float p)
   float	c;
   float	angle;
 
+  if (p <= 0 || p >= 90)
+    {
+      return (-1.0f);
+    }
   angle = p * M_PI / 180;
   a = powf(dir.x, 2) + powf(dir.y, 2) - powf(dir.z, 2) * powf(tanf(angle), 2);
   b = 2 * (pos.x * dir.x
